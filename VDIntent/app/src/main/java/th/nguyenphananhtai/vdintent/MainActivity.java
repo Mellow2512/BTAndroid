bf95332a50d679b2dc287fabc40d03d2eecbb251
Tai <nguyentai3907@gmail.com>
2025-10-22 09:42:17 +0700

    Update

--- VDIntent/app/src/main/java/th/nguyenphananhtai/vdintent/MainActivity.java
+++ VDIntent/app/src/main/java/th/nguyenphananhtai/vdintent/MainActivity.java
@@ -1,6 +1,10 @@
 package th.nguyenphananhtai.vdintent;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -19,6 +23,19 @@ public class MainActivity extends AppCompatActivity {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+        });
+
+        Button nutChuyen = findViewById(R.id.btnSend);
+        nutChuyen.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent iChuyen = new Intent(MainActivity.this, SecondActivity.class);
+                EditText edtHoTen = findViewById(R.id.edtHoTen);
+                String data = edtHoTen.getText().toString();
+                iChuyen.putExtra("HovaTen", data);
+                iChuyen.putExtra("data2", "HongTruong");
+                startActivity(iChuyen);
+            }
         });
     }
 }
