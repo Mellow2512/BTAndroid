dae13ed3df6cc8ba38ce2a1302093805277b4fa1
Tai <nguyentai3907@gmail.com>
2025-10-29 08:05:50 +0700

    Update lan 3

--- QuanLyVatLieu/app/src/main/java/thigk1/nguyenphananhtai/quanlyvatlieu/DienTichActivity.java
+++ QuanLyVatLieu/app/src/main/java/thigk1/nguyenphananhtai/quanlyvatlieu/DienTichActivity.java
@@ -1,24 +1,74 @@
 package thigk1.nguyenphananhtai.quanlyvatlieu;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
+
+import com.google.android.material.textfield.TextInputEditText;
 
 public class DienTichActivity extends AppCompatActivity {
+
+    TextInputEditText tieCD;
+    TextInputEditText tieCR;
+    TextView tvResult;
+    Button btnTinhDienTich;
+
+    ImageButton ibtnBack;
+
+    void QuayLai(){
+        ibtnBack = findViewById(R.id.btnBack);
+
+        ibtnBack.setOnClickListener(v -> {
+            finish();
+        });
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_dien_tich);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+
+        // Gọi hàm khởi tạo sự kiện
+        TinhDiemTB();
+        QuayLai();
+    }
+
+    void TinhDiemTB() {
+        tieCD = findViewById(R.id.tieCD);
+        tieCR = findViewById(R.id.tieCR);
+        tvResult = findViewById(R.id.tvResult);
+        btnTinhDienTich = findViewById(R.id.btnTinhDienTich);
+
+        btnTinhDienTich.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Lấy giá trị khi bấm nút, không lấy trước đó
+                String gkStr = tieCD.getText().toString().trim();
+                String ckStr = tieCR.getText().toString().trim();
+
+                if (gkStr.isEmpty() || ckStr.isEmpty()) {
+                    tvResult.setText("Vui lòng nhập đủ điểm!");
+                    return;
+                }
+
+                try {
+                    float diemGK = Float.parseFloat(gkStr);
+                    float diemCK = Float.parseFloat(ckStr);
+
+                    float diemTB = (diemGK + diemCK) / 2;
+
+                    tvResult.setText("Diện tích: " + diemTB);
+                } catch (NumberFormatException e) {
+                    tvResult.setText("Điểm nhập không hợp lệ!");
+                }
+            }
         });
     }
 }
